4adbc08d3031f1e9d45d139e5e25728d2a029762
npallai <nicholaspallai@hotmail.com>
2018-09-09 22:17:13 -0700

    Added movement for display surface

--- GameOfLife/Main.Designer.cs
+++ GameOfLife/Main.Designer.cs
@@ -29,26 +29,55 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.picBxMain = new System.Windows.Forms.PictureBox();
             this.nextGenTimer = new System.Windows.Forms.Timer(this.components);
-            this.btnStop = new System.Windows.Forms.Button();
             this.lblAge = new System.Windows.Forms.Label();
             this.lblTimerInterval = new System.Windows.Forms.Label();
             this.mTbxInterval = new System.Windows.Forms.MaskedTextBox();
+            this.picBxMain = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picBxMain)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnStart
+            // nextGenTimer
             // 
-            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnStart.Location = new System.Drawing.Point(630, 450);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.Btn_Start_Click);
+            this.nextGenTimer.Interval = 50;
+            this.nextGenTimer.Tick += new System.EventHandler(this.NextGenTimer_Tick);
+            // 
+            // lblAge
+            // 
+            this.lblAge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 456);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(38, 13);
+            this.lblAge.TabIndex = 3;
+            this.lblAge.Text = "Age: 0";
+            // 
+            // lblTimerInterval
+            // 
+            this.lblTimerInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTimerInterval.AutoSize = true;
+            this.lblTimerInterval.Location = new System.Drawing.Point(669, 456);
+            this.lblTimerInterval.Name = "lblTimerInterval";
+            this.lblTimerInterval.Size = new System.Drawing.Size(71, 13);
+            this.lblTimerInterval.TabIndex = 5;
+            this.lblTimerInterval.Text = "Timer Interval";
+            // 
+            // mTbxInterval
+            // 
+            this.mTbxInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.mTbxInterval.HidePromptOnLeave = true;
+            this.mTbxInterval.Location = new System.Drawing.Point(746, 453);
+            this.mTbxInterval.Mask = "0000";
+            this.mTbxInterval.Name = "mTbxInterval";
+            this.mTbxInterval.PromptChar = '-';
+            this.mTbxInterval.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.mTbxInterval.Size = new System.Drawing.Size(41, 20);
+            this.mTbxInterval.TabIndex = 6;
+            this.mTbxInterval.Text = "200";
+            this.mTbxInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mTbxInterval.ValidatingType = typeof(int);
             // 
             // picBxMain
             // 
@@ -61,72 +90,43 @@
             this.picBxMain.Size = new System.Drawing.Size(775, 432);
             this.picBxMain.TabIndex = 1;
             this.picBxMain.TabStop = false;
+            this.picBxMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picBxMain_MouseClick);
             // 
-            // nextGenTimer
+            // label1
             // 
-            this.nextGenTimer.Interval = 50;
-            this.nextGenTimer.Tick += new System.EventHandler(this.NextGenTimer_Tick);
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(311, 447);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Click Surface to Start and Stop";
             // 
-            // btnStop
+            // label2
             // 
-            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnStop.Enabled = false;
-            this.btnStop.Location = new System.Drawing.Point(712, 450);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(75, 23);
-            this.btnStop.TabIndex = 2;
-            this.btnStop.Text = "Stop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.Btn_Stop_Click);
-            // 
-            // lblAge
-            // 
-            this.lblAge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.lblAge.AutoSize = true;
-            this.lblAge.Location = new System.Drawing.Point(12, 450);
-            this.lblAge.Name = "lblAge";
-            this.lblAge.Size = new System.Drawing.Size(38, 13);
-            this.lblAge.TabIndex = 3;
-            this.lblAge.Text = "Age: 0";
-            // 
-            // lblTimerInterval
-            // 
-            this.lblTimerInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblTimerInterval.AutoSize = true;
-            this.lblTimerInterval.Location = new System.Drawing.Point(506, 455);
-            this.lblTimerInterval.Name = "lblTimerInterval";
-            this.lblTimerInterval.Size = new System.Drawing.Size(71, 13);
-            this.lblTimerInterval.TabIndex = 5;
-            this.lblTimerInterval.Text = "Timer Interval";
-            // 
-            // mTbxInterval
-            // 
-            this.mTbxInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.mTbxInterval.HidePromptOnLeave = true;
-            this.mTbxInterval.Location = new System.Drawing.Point(583, 452);
-            this.mTbxInterval.Mask = "0000";
-            this.mTbxInterval.Name = "mTbxInterval";
-            this.mTbxInterval.PromptChar = '-';
-            this.mTbxInterval.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.mTbxInterval.Size = new System.Drawing.Size(41, 20);
-            this.mTbxInterval.TabIndex = 6;
-            this.mTbxInterval.Text = "200";
-            this.mTbxInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.mTbxInterval.ValidatingType = typeof(int);
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 463);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Arrow Keys to Move";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 485);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.mTbxInterval);
             this.Controls.Add(this.lblTimerInterval);
             this.Controls.Add(this.lblAge);
-            this.Controls.Add(this.btnStop);
             this.Controls.Add(this.picBxMain);
-            this.Controls.Add(this.btnStart);
+            this.KeyPreview = true;
             this.Name = "Main";
             this.Text = "Game Of Life";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.picBxMain)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -134,13 +134,13 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.PictureBox picBxMain;
         private System.Windows.Forms.Timer nextGenTimer;
-        private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Label lblAge;
         private System.Windows.Forms.Label lblTimerInterval;
         private System.Windows.Forms.MaskedTextBox mTbxInterval;
+        private System.Windows.Forms.PictureBox picBxMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- GameOfLife/Main.cs
+++ GameOfLife/Main.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -9,92 +8,16 @@ namespace GameOfLife
     {
         private Universe _universe;
         private Bitmap _map;
+        private long _xOffset = 0;
+        private long _yOffset = 0;
+        private bool _started = false;
 
         public Main()
         {
             InitializeComponent();
 
-            _universe = new Universe(GetTestingCells());
+            _universe = new Universe(StartingCells.GetStartingCells());
             _universe.OnCellIsAlive += Universe_OnCellIsAlive;
-        }
-
-        private List<Cell> GetTestingCells()
-        {
-            List<Cell> livingCells = new List<Cell>
-            {
-                new Cell(100, 100), //Block
-                new Cell(101, 100),
-                new Cell(10, 101),
-                new Cell(101, 101),
-                new Cell(200, 200), //Blinker
-                new Cell(200, 201),
-                new Cell(200, 202),
-                new Cell(300, 300), //Glider
-                new Cell(301, 300),
-                new Cell(302, 300),
-                new Cell(302, 299),
-                new Cell(301, 298),
-                new Cell(400, 300), //Glider
-                new Cell(401, 300),
-                new Cell(402, 300),
-                new Cell(402, 299),
-                new Cell(401, 298),
-                new Cell(500, 300), //Glider
-                new Cell(501, 300),
-                new Cell(502, 300),
-                new Cell(502, 299),
-                new Cell(501, 298),
-                new Cell(-200, -100), //Glider
-                new Cell(-199, -100),
-                new Cell(-198, -100),
-                new Cell(-198, -101),
-                new Cell(-199, -102),
-                new Cell(600, 200), //Pentadecathlon
-                new Cell(601, 200),
-                new Cell(602, 200),
-                new Cell(601, 201),
-                new Cell(601, 202),
-                new Cell(600, 203),
-                new Cell(601, 203),
-                new Cell(602, 203),
-                new Cell(600, 205),
-                new Cell(601, 205),
-                new Cell(602, 205),
-                new Cell(600, 206),
-                new Cell(601, 206),
-                new Cell(602, 206),
-                new Cell(600, 208),
-                new Cell(601, 208),
-                new Cell(602, 208),
-                new Cell(601, 209),
-                new Cell(601, 210),
-                new Cell(600, 211),
-                new Cell(601, 211),
-                new Cell(602, 211),
-                new Cell(300, 200), //Pentadecathlon
-                new Cell(301, 200),
-                new Cell(302, 200),
-                new Cell(301, 201),
-                new Cell(301, 202),
-                new Cell(300, 203),
-                new Cell(301, 203),
-                new Cell(302, 203),
-                new Cell(300, 205),
-                new Cell(301, 205),
-                new Cell(302, 205),
-                new Cell(300, 206),
-                new Cell(301, 206),
-                new Cell(302, 206),
-                new Cell(300, 208),
-                new Cell(301, 208),
-                new Cell(302, 208),
-                new Cell(301, 209),
-                new Cell(301, 210),
-                new Cell(300, 211),
-                new Cell(301, 211)
-            };
-
-            return livingCells;
         }
 
         private void NextGenTimer_Tick(object sender, EventArgs e)
@@ -114,27 +37,58 @@ namespace GameOfLife
 
         private void Universe_OnCellIsAlive(object sender, CellEventArgs e)
         {
-            if (e.XLocation >= _map.Width || e.XLocation < 0 || e.YLocation >= _map.Height || e.YLocation < 0)
+            var xPixelToSet = e.XLocation - _xOffset;
+            var yPixelToSet = e.YLocation - _yOffset;
+
+            if (e.XLocation >= _xOffset + _map.Width || e.XLocation < _xOffset || e.YLocation >= _yOffset + _map.Height || e.YLocation < _yOffset)
             {
                 return;
             }
 
-            _map.SetPixel(unchecked((int)e.XLocation), unchecked((int)e.YLocation), Color.Black);
+            try
+            {
+                _map.SetPixel((int)(xPixelToSet), (int)(yPixelToSet), Color.Black);
+            }
+            catch
+            { }
         }
 
-        private void Btn_Start_Click(object sender, EventArgs e)
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    _xOffset--;
+                    break;
+                case Keys.Right:
+                    _xOffset++;
+                    break;
+                case Keys.Up:
+                    _yOffset++;
+                    break;
+                case Keys.Down:
+                    _yOffset--;
+                    break;
+            }
+
+            picBxMain.Image = _map;
+        }
+
+        private void picBxMain_MouseClick(object sender, MouseEventArgs e)
         {
             nextGenTimer.Interval = int.Parse(mTbxInterval.Text);
-            nextGenTimer.Start();
-            btnStop.Enabled = true;
-            btnStart.Enabled = false;
-        }
 
-        private void Btn_Stop_Click(object sender, EventArgs e)
-        {
-            nextGenTimer.Stop();
-            btnStart.Enabled = true;
-            btnStop.Enabled = false;
+            if (_started)
+            {
+                nextGenTimer.Stop();
+            }
+            else
+            {
+                nextGenTimer.Start();
+            }
+
+            _started = !_started;
+            mTbxInterval.Enabled = !mTbxInterval.Enabled;
         }
     }
 }
